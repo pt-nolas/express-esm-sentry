23190fc79eaa16fde4f63f69aaddd79f97d91ce7
229803497+pt-nolas@users.noreply.github.com <229803497+pt-nolas@users.noreply.github.com>
2025-09-19 20:33:52 +0300

    add dropspan integration

--- instrument.mjs
+++ instrument.mjs
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 // Ensure to call this before importing any other modules!
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: "https://3cfb20aa79cd08560cea68d7df46a3f4@o4509915707604992.ingest.de.sentry.io/4510019134947408",
   // Add Tracing by setting tracesSampleRate
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
